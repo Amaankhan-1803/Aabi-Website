170a08b1c09fb2948551b30360ba00d2f98e4410
Salman Shaikh <coder88salman@gmail.com>
2025-02-11 14:24:07 +0530

    many chanegs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -20,18 +20,21 @@ const Footer = () => {
                Contact
             </h5>
             <ul className='contact-list'>
-              <li><a href="" className='contact-item'><i class="bi bi-telephone pe-2"></i> +968-99719361</a></li>
-              <li><a href="" className='contact-item'><i class="bi bi-whatsapp pe-2"></i> +968-79955998</a></li>
-              <li><a href="" className='contact-item'><i class="bi bi-envelope pe-2"></i> sales@abunidhaloman.com</a></li>
-              <li><a href="" className='contact-item'><i class="bi bi-geo-alt pe-2"></i> 289H+33G, Jalan Bani Buali</a></li>
+                <li><a href="tel:+968-99719361" className='contact-item'><i
+                    className="bi bi-telephone pe-2"></i> +968-99719361</a></li>
+                <li><a href="https://wa.me/96879955998" className='contact-item'><i
+                    className="bi bi-whatsapp pe-2"></i> +968-79955998</a></li>
+                <li><a href="" className='contact-item'><i className="bi bi-envelope pe-2"></i> sales@abunidhaloman.com</a>
+                </li>
+                {/*<li><a href="" className='contact-item'><i class="bi bi-geo-alt pe-2"></i> 289H+33G, Jalan Bani Buali</a></li>*/}
             </ul>
         </div>
-        <div className="col-md-4 abtus ps-md-5">
-            <h5 className="head">
-               Connect With Us
-            </h5>
-            <ul className='social-list d-flex align-items-center gap-4'>
-              <li><a href="https://www.instagram.com/abu_nidhal_jewellry1?igsh=MXA5MXdhYW94amZpNg==" className='social-item'><i class="bi bi-instagram "></i></a></li>
+          <div className="col-md-4 abtus ps-md-5">
+              <h5 className="head">
+                  Connect With Us
+              </h5>
+              <ul className='social-list d-flex align-items-center gap-4'>
+                  <li><a href="https://www.instagram.com/abu_nidhal_jewellry1?igsh=MXA5MXdhYW94amZpNg==" className='social-item'><i class="bi bi-instagram "></i></a></li>
               <li><a href="https://www.snapchat.com/add/abunidhal1?sender_web_id=c8696c6b-61ea-4500-9f85-d0bf914457b8" className='social-item'><i class="bi bi-snapchat "></i></a></li>
               <li><a href="https://maps.app.goo.gl/mB5R542T1Cqseoh5A?g_st=aw" className='social-item'><i class="bi bi-geo-alt "></i></a></li>
             </ul>
