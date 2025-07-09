4637ee3b2e69cfbdfb175ff3c5a6acedd7cd5a0f
Salman Shaikh <coder88salman@gmail.com>
2025-07-10 02:52:28 +0530

    Revert "pri"
    This reverts commit dc38d14bc455e1387cd8aa9ac373e4ad17d41c1e.

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,43 @@
-import { Routes, Route } from 'react-router-dom'
-     import Navbar from './components/Navbar'
-     import Slider from './components/Sliders'
-     import InstaPost from './components/Instapost'
-     import Aboutus from './components/aboutus'
-     import Footer from './components/Footer'
-     import PrivacyPolicy from './components/PrivacyPolicy'
+import { useState } from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+import Slider from './components/Sliders'
+import Collection from './components/Collection'
+import InstaPost from './components/Instapost'
+import Aboutus from './components/aboutus'
+import Social from './components/social'
+import Footer from './components/Footer'
+function App() {
+  const [count, setCount] = useState(0)
 
-     function App() {
-       return (
-           <>
-               <div className="navbars">
-                   <Navbar/>
-               </div>
-               <Routes>
+  return (
+    <>
 
-                   <Route path="/" element={
-                       <>
+   <div className="navbars">
+   <Navbar/>
+   </div>
 
-                           <div className="container-fluid carousel_slider">
-                               <Slider/>
-                           </div>
-                           <div className="container-fluid instaposts">
-                               <InstaPost/>
-                           </div>
-                           <div className="container-fluid aboutus">
-                               <Aboutus/>
-                           </div>
-                           <div className="container-fluid footer">
-                               <Footer/>
-                           </div>
-                       </>
-                   }/>
-                   <Route path="/privacy-policy" element={<PrivacyPolicy/>}/>
-               </Routes>
-           </>
-       )
-     }
+     <div className="container-fluid carousel_slider">
+      <Slider/>
+     </div>
+     {/*<div className="container-fluid collection">*/}
+     {/* <Collection/>*/}
+     {/*</div>*/}
+     <div className="container-fluid instaposts">
+      <InstaPost/>
+     </div>
+     <div className="container-fluid aboutus">
+      <Aboutus/>
+     </div>
+     {/*<div className="container-fluid social">*/}
+     {/* <Social/>*/}
+     {/*</div>*/}
+     <div className="container-fluid footer">
+      <Footer/>
+     </div>
+
+    </>
+  )
+}
 
 export default App
