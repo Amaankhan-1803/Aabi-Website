dc38d14bc455e1387cd8aa9ac373e4ad17d41c1e
Salman Shaikh <coder88salman@gmail.com>
2025-07-10 02:36:49 +0530

    pri

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import * as ReactGA from "react-ga";
@@ -8,7 +9,9 @@ ReactGA.initialize('G-GHMWG28MPC');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </StrictMode>,
 )
