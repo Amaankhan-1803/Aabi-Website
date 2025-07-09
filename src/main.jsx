0ad7c2135dcf2079c65c93dbad474520130ede09
Salman Shaikh <coder88salman@gmail.com>
2025-07-10 02:47:44 +0530

    Revert "many chanegs"
    This reverts commit fb0a24720bbb88dc9013a6f57b6f4fd290f8a60d.

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import * as ReactGA from "react-ga";
-
-ReactGA.initialize('G-GHMWG28MPC');
-ReactGA.pageview(window.location.pathname + window.location.search);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
